4514c248b46e9c5930c58d5666327dc468232e38
tuanvnpt <anhtuan.ttkd@gmail.com>
2015-10-15 22:14:06 +0700

    Update chongcopy.js

--- chongcopy.js
+++ chongcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://giaiphapthuonghieu.vn'>http://giaiphapthuonghieu.vn"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://vnptvina.com/'>http://vnptvina.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
